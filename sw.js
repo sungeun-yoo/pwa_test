d5963161fbd648efffba87eb6de35220466f3642
se.yoo <yoose0727@gmail.com>
2025-03-10 17:37:51 +0900

    First commit

---
+++ sw.js
@@ -0,0 +1,73 @@
+const CACHE_NAME = 'pwa-camera-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png'
+];
+
+// 서비스 워커 설치 및 캐시
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('캐시 열기 성공');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 캐시 또는 네트워크에서 리소스 가져오기
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 캐시에서 찾으면 반환
+        if (response) {
+          return response;
+        }
+        // 없으면 네트워크에서 가져오기
+        return fetch(event.request)
+          .then(response => {
+            // 유효한 응답이 아니면 그냥 반환
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // 응답 복제 (스트림은 한 번만 사용 가능)
+            const responseToCache = response.clone();
+
+            // 네트워크 응답을 캐시에 저장
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          });
+      })
+  );
+});
+
+// 이전 버전의 캐시 삭제
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
